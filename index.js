3de9f95c39d81b5f8d14a3a778e8eb371799e3d3
Saint-Jean Germila <germila@noukod.com>
2019-03-19 16:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Driver{
-  construtor(name, string){
-    this.name = name
-    this.string = string
-  }
-   startDate(){
+    constructor(name,startDate){
+        this.name = name;
+        this.startDate = new Date(startDate);
+    }
 
-
-     }
-     yearsExperienceFromBeginningOf(year){
-
-     }
-  }
+    yearsExperienceFromBeginningOf (endYear){
+        return endYear - this.startDate.getFullYear() - 1;
+    }
+}
 
 class Route {
-  constructor(beginningLocation, endingLocation) {
-    blocksTravelled(){
+    constructor(beginningLocation, endingLocation){
+      this.beginningLocation = beginningLocation
+      this.endingLocation = endingLocation
+  }
 
+  blocksTravelled(){
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+    let horizontalDistance = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
 
+    if(this.beginningLocation.horizontal === this.endingLocation.horizontal){
+        return verticalDistance;
+    }else if (this.beginningLocation.vertical === this.endingLocation.vertical){
+        return horizontalDistance;
+    }else{
+        return verticalDistance + horizontalDistance;
+    }
+  }
+
+  estimatedTime(peak){
+    if(peak){
+        return this.blocksTravelled()/2
+    } else {
+        return this.blocksTravelled()/3
     }
   }
 }
